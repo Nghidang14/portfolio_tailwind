affa1a1630381f7780ecd3f2e2df150d4d1cc10d
NghiXu <nghidk.mp@gmail.com>
2024-10-03 22:25:33 +0700

    final

--- src/Components/PortfolioSection/PortfolioSection.tsx
+++ src/Components/PortfolioSection/PortfolioSection.tsx
@@ -23,7 +23,7 @@ const portfolioItems = [
     {
         title: 'Line Runner',
         description: 'The second Android mini game which I learned in Unity.',
-        link: 'https://drive.google.com/file/d/1D_7uYKMHF7ER0q5TXtOv8PeUmdcTx5Wg/view?usp=sharing',
+        link: 'https://drive.google.com/file/d/1CcNb5b_QKzUZ6bvDk7d3zLTOwb9WAXbM/view?usp=sharing',
         imagePath: '/images/Fake_Screen_Running.PNG',
     }
     
